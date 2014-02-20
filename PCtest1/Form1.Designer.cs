639f51c678a08186af66185cf841b8efc2f61e00
alafazam <alafazam@gmail.com>
2014-02-21 02:21:27 +0530

    this is what i have Learned so far

---
+++ PCtest1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PCtest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn.Location = new System.Drawing.Point(149, 133);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(132, 38);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "Start";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 303);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.Text = "Can you read me";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ PCtest1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vJoyInterfaceWrap;
+namespace PCtest1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        static public vJoy joystick;
+        static public uint id = 15;
+        static public void start()
+        {
+            joystick = new vJoy();
+
+            vJoy.ResetButtons(id);
+           
+           
+        }
+
+
+        static void Main(string[] args)
+        {
+           
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+           
+         
+
+        }
+    }
+}
