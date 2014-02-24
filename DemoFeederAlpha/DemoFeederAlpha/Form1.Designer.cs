8ef1e6bc3f73f7d67c6b25ceb686cf150db6a629
alafazam <alafazam@gmail.com>
2014-02-25 02:13:44 +0530

    Latest alpha.. model..

---
+++ DemoFeederAlpha/DemoFeederAlpha/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace DemoFeederAlpha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(13, 13);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 13);
+            this.lbl.TabIndex = 1;
+            this.lbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn1.Font = new System.Drawing.Font("Orator Std", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btn1.Location = new System.Drawing.Point(175, 294);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(197, 58);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Start Feeding";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 407);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lbl);
+            this.Name = "Form1";
+            this.Text = "AlphaFeeder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        public System.Windows.Forms.Label lbl;
+    }
+}
+
---
+++ DemoFeederAlpha/DemoFeederAlpha/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vJoyInterfaceWrap;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace DemoFeederAlpha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl.Text += "HELLO";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        static int GetIntegerFromBinaryString(string binary, int bitCount)
+        {
+            if (binary.Length == bitCount && binary[0] == '1')
+                return Convert.ToInt32(binary.PadLeft(32, '1'), 2);
+            else
+                return Convert.ToInt32(binary, 2);
+        }
+
+        public static void StartFeeding()
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            vJoy joystick;
+            vJoy.JoystickState iReport;
+            iReport = new vJoy.JoystickState();
+            uint id = 15;
+            joystick = new vJoy();
+            joystick.AcquireVJD(id);
+            joystick.ResetAll();
+
+            int localPort = 6000
+             , bufferSize = 9;
+            //Buffer size is 9 beacause 2 extra bytes are recieved..
+            IPAddress localAddress = IPAddress.Any;
+            SocketType sockType = SocketType.Stream;
+            ProtocolType sockProtocol = ProtocolType.Tcp;
+            Socket serverSocket = null;
+            lbl.Text += "Reacing try block";
+            try
+            {
+                IPEndPoint localEndPoint = new IPEndPoint(localAddress, localPort);
+                Socket clientSocket;
+                byte[] receiveBuffer = new byte[bufferSize];
+                int rc;
+                // Create the server socket
+                serverSocket = new Socket(localAddress.AddressFamily, sockType, sockProtocol);
+                // Bind the socket to the local interface specified
+                serverSocket.Bind(localEndPoint);
+                serverSocket.Listen(1);
+                // Wait for a client connection
+                clientSocket = serverSocket.Accept();
+                lbl.Text += "\n" + "Server Started";
+                while (true)
+                {
+                    clientSocket.Receive(receiveBuffer);
+                    lbl.Text += "Heap size reciever" + receiveBuffer.Length + "\n";
+                    iReport = new vJoy.JoystickState();
+                    //main parsing and feeding
+                    iReport.bDevice = (byte)id;
+                    iReport.AxisX = 128 * (int)receiveBuffer[3] * 2;
+                    iReport.AxisY = 128 * (int)receiveBuffer[4] * 2;
+                    iReport.AxisZ = 128 * (int)receiveBuffer[5] * 2;
+                    iReport.AxisZRot = (int)receiveBuffer[6];
+
+                    // Set buttons one by one
+                    iReport.Buttons = (uint)(receiveBuffer[1] + 512 * (receiveBuffer[2] % 2) + 1024 * (receiveBuffer[2] / 2 % 2));
+                    int pov = receiveBuffer[2] / 4;
+                    iReport.bHats = (uint)pov;
+                    joystick.UpdateVJD(id, ref iReport);
+                }
+            }
+            catch (SocketException err)
+            { // Console.WriteLine("Server: Socket error occurred: {0}", err.Message);
+            }
+            finally { if (serverSocket != null)serverSocket.Close(); }
+
+
+            //end of startfeeding
+
+
+            //end of class
+        }
+    }
+}
