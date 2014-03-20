bc4f17de4863afc1c1a4b52a8730d9a4d05d2c94
alafazam <alafazam@gmail.com>
2014-03-20 18:53:10 +0530

    New About Added,sensivity factor mechanism removed

--- DemoFeederAlpha/DemoFeederAlpha/Form1.Designer.cs
+++ DemoFeederAlpha/DemoFeederAlpha/Form1.Designer.cs
@@ -29,40 +29,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbl = new System.Windows.Forms.Label();
             this.btn1 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tab1 = new System.Windows.Forms.TabPage();
+            this.tb1 = new System.Windows.Forms.TextBox();
             this.tab2 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tab1.SuspendLayout();
             this.tab2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lbl
-            // 
-            this.lbl.AutoSize = true;
-            this.lbl.BackColor = System.Drawing.Color.White;
-            this.lbl.Location = new System.Drawing.Point(3, 3);
-            this.lbl.MinimumSize = new System.Drawing.Size(630, 290);
-            this.lbl.Name = "lbl";
-            this.lbl.Size = new System.Drawing.Size(630, 290);
-            this.lbl.TabIndex = 1;
-            this.lbl.Text = "Events Log";
             // 
             // btn1
             // 
             this.btn1.BackColor = System.Drawing.SystemColors.HotTrack;
             this.btn1.Font = new System.Drawing.Font("Orator Std", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn1.ForeColor = System.Drawing.Color.AliceBlue;
-            this.btn1.Location = new System.Drawing.Point(34, 399);
+            this.btn1.Location = new System.Drawing.Point(38, 399);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(197, 58);
             this.btn1.TabIndex = 2;
@@ -78,44 +63,43 @@
             this.tabControl1.Location = new System.Drawing.Point(34, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(648, 351);
+            this.tabControl1.Size = new System.Drawing.Size(644, 351);
             this.tabControl1.TabIndex = 3;
             // 
             // tab1
             // 
             this.tab1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.tab1.Controls.Add(this.lbl);
+            this.tab1.Controls.Add(this.tb1);
             this.tab1.Location = new System.Drawing.Point(4, 44);
             this.tab1.Name = "tab1";
             this.tab1.Padding = new System.Windows.Forms.Padding(3);
-            this.tab1.Size = new System.Drawing.Size(640, 303);
+            this.tab1.Size = new System.Drawing.Size(636, 303);
             this.tab1.TabIndex = 0;
             this.tab1.Text = "Log";
+            // 
+            // tb1
+            // 
+            this.tb1.AcceptsReturn = true;
+            this.tb1.Enabled = false;
+            this.tb1.Location = new System.Drawing.Point(330, 6);
+            this.tb1.MinimumSize = new System.Drawing.Size(300, 200);
+            this.tb1.Multiline = true;
+            this.tb1.Name = "tb1";
+            this.tb1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb1.Size = new System.Drawing.Size(300, 200);
+            this.tb1.TabIndex = 2;
             // 
             // tab2
             // 
             this.tab2.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.tab2.Controls.Add(this.label3);
-            this.tab2.Controls.Add(this.label2);
-            this.tab2.Controls.Add(this.label1);
-            this.tab2.Controls.Add(this.trackBar1);
-            this.tab2.Controls.Add(this.button1);
+            this.tab2.Controls.Add(this.pictureBox1);
             this.tab2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tab2.Location = new System.Drawing.Point(4, 44);
             this.tab2.Name = "tab2";
             this.tab2.Padding = new System.Windows.Forms.Padding(3);
-            this.tab2.Size = new System.Drawing.Size(640, 303);
+            this.tab2.Size = new System.Drawing.Size(636, 303);
             this.tab2.TabIndex = 1;
-            this.tab2.Text = "Settings";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(534, 257);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(73, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Apply";
-            this.button1.UseVisualStyleBackColor = true;
+            this.tab2.Text = "About";
             // 
             // btn2
             // 
@@ -130,47 +114,22 @@
             this.btn2.UseVisualStyleBackColor = false;
             this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
-            // trackBar1
+            // pictureBox1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(26, 72);
-            this.trackBar1.Maximum = 5;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(258, 45);
-            this.trackBar1.TabIndex = 1;
-            this.trackBar1.Value = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 108);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "1x";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(248, 108);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "10x";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 49);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 20);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Sensivity of analogs";
+            this.pictureBox1.Image = global::DemoFeederAlpha.Properties.Resources.aaaa;
+            this.pictureBox1.Location = new System.Drawing.Point(10, -52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(620, 389);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(719, 520);
+            this.ClientSize = new System.Drawing.Size(718, 467);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn1);
             this.Controls.Add(this.tabControl1);
@@ -181,7 +140,7 @@
             this.tab1.PerformLayout();
             this.tab2.ResumeLayout(false);
             this.tab2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -189,16 +148,13 @@
         #endregion
 
         private System.Windows.Forms.Button btn1;
-        public System.Windows.Forms.Label lbl;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tab1;
         private System.Windows.Forms.TabPage tab2;
         private System.Windows.Forms.Button btn2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TrackBar trackBar1;
+        protected System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        
     }
 }
 
--- DemoFeederAlpha/DemoFeederAlpha/Form1.cs
+++ DemoFeederAlpha/DemoFeederAlpha/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,42 +17,47 @@ namespace DemoFeederAlpha
     public partial class Form1 : Form
     {
         feeder feeder1;
+        int sensivity_factor;
         public Form1()
         {
 
             InitializeComponent();
-            lbl.Text += "\nHELLO";
+            tb1.Text += "Hello User \r\n";
             feeder1 = new feeder();
             UpdateMyStatus(feeder1.getStatus());
-            
+            String[] asd = new String[9];
+
+            asd = feeder1.LogDetails();
+            tb1.Text += "X Axis " + asd[0] + "\r\n";
+            tb1.Text += "Y Axis " + asd[1] + "\r\n";
+            tb1.Text += "Z Axis " + asd[2] + "\r\n";
+            tb1.Text += "RX Axis " + asd[3] + "\r\n";
+            tb1.Text += "RZ Axis " + asd[4] + "\r\n";
+            tb1.Text += "No of Buttons " + asd[5] + "\r\n";
+            tb1.Text += "No of Constant POV " + asd[6] + "\r\n";
+            tb1.Text += "No of Discrete POV " + asd[7] + "\r\n";
+            tb1.Text += "Press Start feeding to Start " +  "\r\n";
+
         }
 
 
         public void updateLog(String AddThisToLog)
         {
-            this.lbl.Text += "\n" + AddThisToLog;
+            this.tb1.Text += "\r\n" + AddThisToLog;
         }
+
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            if (feeder1._firstRun)
+            if (!feeder1._keepRunning)
             {
                 ThreadPool.QueueUserWorkItem(new WaitCallback(feeder1.InitFeeding));
-                lbl.Text += "\nfirst run";
+                tb1.Text += "Feeding Started" +" \r\n";
             }
             else
             {
-                if (!feeder1._keepRunning)
-                {
-                    ThreadPool.QueueUserWorkItem(new WaitCallback(feeder1.InitFeeding));
-                    lbl.Text += "\n Feeding restarted";
-                }
-                else
-                {
-                    lbl.Text += "\nfeeder alreading feeding";
-                }
+                tb1.Text +="Feeder alreading feeding \r\n";
             }
-        
         }
 
         private void btn2_Click(object sender, EventArgs e)
@@ -59,37 +65,52 @@ namespace DemoFeederAlpha
             if (feeder1._keepRunning)
             {
                 feeder1.StopFeeding();
-                lbl.Text += "\n" + "feeding stoped ";
+                tb1.Text += "\r\n" + "Feeding stoped ";
             }
-            
+
         }
 
-        
+
         private void UpdateMyStatus(String status)
         {
 
             switch (status)
             {
                 case "VJD_STAT_OWN":
-                    lbl.Text += "vJoy Device  is already owned by this feeder\n";
+                    tb1.Text += "vJoy Device  is already owned by this feeder \r\n";
                     break;
                 case "VJD_STAT_FREE":
-                    lbl.Text += "\nvJoy Device  is free and can be started";
+                    tb1.Text += "vJoy Device  is free and can be started \r\n";
                     break;
                 case "VJD_STAT_BUSY":
-                    lbl.Text += "vJoy Device  is already owned by another feeder\nCannot continue\n";
+                    tb1.Text += "vJoy Device  is already owned by another feeder\r\nCannot continue\r\n";
                     return;
                 case "VJD_STAT_MISS":
-                    lbl.Text += "vJoy Device  is not installed or disabled\nCannot continue\n";
+                    tb1.Text += "vJoy Device  is not installed or disabled\r\nCannot continue\r\n";
                     return;
                 default:
-                    lbl.Text += "vJoy Device general error\nCannot continue\n";
+                    tb1.Text += "vJoy Device general error\r\nCannot continue\r\n";
                     return;
             };
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            feeder1.StopFeeding();
+            feeder1.applyChanges(sensivity_factor);
+            tb1.Text += "Changes Applied please restart feeding";
+        }
 
-        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("GameLad Pc App 1.0 \nWritten by:Alaf Azam Khan & Ankit Verma\n'Hope you have fun'\nFeedbacks are Welcome", "About");
+        }
+
+       
+
+
+
+
 
 
     }
@@ -104,8 +125,7 @@ namespace DemoFeederAlpha
     {
         vJoy joystick;
         uint id = 15;
-        int localPort = 6000
-        , bufferSize = 7;
+        int localPort = 6000, bufferSize = 7;
         IPAddress localAddress = IPAddress.Any;
         SocketType sockType = SocketType.Stream;
         ProtocolType sockProtocol = ProtocolType.Tcp;
@@ -116,21 +136,27 @@ namespace DemoFeederAlpha
         Socket clientSocket;
         long maxval = 0;
         public volatile bool _keepRunning = false;
-        public volatile bool _firstRun = true;
-       
-
+        public float conversionFactor;
+        private int sensivityFactor = 2;
 
         public feeder()
         {
             localEndPoint = new IPEndPoint(localAddress, localPort);
-            // Create the server socket
-            //start the server object
+            // Create the server socket 
             serverSocket = new Socket(localAddress.AddressFamily, sockType, sockProtocol);
             // Bind the socket to the local interface specified
             serverSocket.Bind(localEndPoint);
-            
+            //create the joystick handler
             joystick = new vJoy();
+            //set value for max_of_Axis
             joystick.GetVJDAxisMax(id, HID_USAGES.HID_USAGE_X, ref maxval);
+            conversionFactor = maxval / 255;
+
+        }
+
+        public void applyChanges(int s)
+        {
+            sensivityFactor = s;
         }
 
 
@@ -145,79 +171,71 @@ namespace DemoFeederAlpha
         public void InitFeeding(object state)
         {
             iReport = new vJoy.JoystickState();
+            //Acquire Vjoystick Driver
             joystick.AcquireVJD(id);
+            //reset vjoy driver
             joystick.ResetAll();
-
-          
-            _firstRun = false;
-            
-         
             this.StartFeeding(new object());
         }
-
-
 
         public void StartFeeding(object state)
         {
             byte[] receiveBuffer = new byte[bufferSize];
             _keepRunning = true;
-           
             // Wait for a client connection
             this.serverSocket.Listen(1);
 
-            
-        try
-        {   
-            
-            clientSocket = serverSocket.Accept();
-            this.LogDetails();
-            //lbl.Text += "\n" + "Server Started";
-            while (_keepRunning)
+            try
             {
-                clientSocket.Receive(receiveBuffer);
-                // lbl.Text += "Heap size reciever" + receiveBuffer.Length + "\n";
-                iReport = new vJoy.JoystickState();
-                //main parsing and feeding
-                iReport.bDevice = (byte)id;
-                iReport.AxisX = (int)((maxval / 255) * (int)receiveBuffer[3]);
-                iReport.AxisY = (int)((maxval / 255) * (int)receiveBuffer[4]);
-                iReport.AxisZ = (int)((maxval / 255) * (int)receiveBuffer[5]);
-                iReport.AxisZRot = (int)((maxval / 255) * (int)receiveBuffer[6]);
 
-                // Set buttons one by one
-                iReport.Buttons = (uint)(receiveBuffer[1] + 256 * (receiveBuffer[2] % 2) + 512 * ((receiveBuffer[2] / 2) % 2));
-                int pov = (int)receiveBuffer[2] >> 2;
-                //if (pov == 4)
-                iReport.bHats = 0xFFFFFFFF;
-                //else
-                //  iReport.bHats = (uint)pov;
-                joystick.UpdateVJD(id, ref iReport);
+                clientSocket = serverSocket.Accept();
+                while (_keepRunning)
+                {
+                    clientSocket.Receive(receiveBuffer);
+                    //creating Ireport for feeding
+                    iReport = new vJoy.JoystickState();
+                    //main parsing and feeding
+                    iReport.bDevice = (byte)id;
+                    iReport.AxisX = (int)(conversionFactor * ((int)receiveBuffer[3] / sensivityFactor * sensivityFactor + sensivityFactor - 1));
+                    iReport.AxisY = (int)(conversionFactor * ((int)receiveBuffer[4] / sensivityFactor * sensivityFactor + sensivityFactor - 1));
+                    iReport.AxisZ = (int)(conversionFactor * ((int)receiveBuffer[5] / sensivityFactor * sensivityFactor + sensivityFactor - 1));
+                    iReport.AxisZRot = (int)(conversionFactor * ((int)receiveBuffer[6] / sensivityFactor * sensivityFactor + sensivityFactor - 1));
+                    // Set buttons one by one
+                    iReport.Buttons = (uint)(receiveBuffer[1] + 256 * (receiveBuffer[2] % 2) + 512 * ((receiveBuffer[2] / 2) % 2));
+                    int pov = (int)receiveBuffer[2] >> 2;
+                    //if (pov == 4)
+                    iReport.bHats = 0xFFFFFFFF;
+                    //else
+                    //  iReport.bHats = (uint)pov;
+                    joystick.UpdateVJD(id, ref iReport);
+                }
+
             }
-
-        }
-        catch (SocketException err)
-        {
-            if (err.Message == "An existing connection was forcibly closed by the remote host")
+            catch (SocketException err)
             {
-                serverSocket.Dispose();
-                this.InitFeeding(new object() );
+                if (err.Message == "An existing connection was forcibly closed by the remote host")
+                {
+                    serverSocket.Dispose();
+                    this.InitFeeding(new object());
+                }
+
+                // Console.WriteLine("Server: Socket error occurred: {0}", err.Message);
             }
-            
-            // Console.WriteLine("Server: Socket error occurred: {0}", err.Message);
-        }
-        finally {
+            finally
+            {
 
- //           serverSocket.Dispose();
-            clientSocket.Dispose();         
+                //serverSocket.Dispose();
+                clientSocket.Dispose();
 
 
-        }
+            }
 
 
             //end of startfeeding
         }
 
-        public void LogDetails()
+
+        public String[] LogDetails()
         {
 
             bool AxisX = joystick.GetVJDAxisExist(id, HID_USAGES.HID_USAGE_X);
@@ -229,9 +247,20 @@ namespace DemoFeederAlpha
             int nButtons = joystick.GetVJDButtonNumber(id);
             int ContPovNumber = joystick.GetVJDContPovNumber(id);
             int DiscPovNumber = joystick.GetVJDDiscPovNumber(id);
+            String[] asd;
+            asd = new String[9];
+            asd[0] = "" + AxisX;
+            asd[1] = "" + AxisY;
+            asd[2] = "" + AxisZ;
+            asd[3] = "" + AxisRX;
+            asd[4] = "" + AxisRZ;
+            asd[5] = "" + nButtons;
+            asd[6] = "" + ContPovNumber;
+            asd[7] = "" + DiscPovNumber;
 
-
+            return asd;
         }
+
         public void StopFeeding()
         {
             _keepRunning = false;
